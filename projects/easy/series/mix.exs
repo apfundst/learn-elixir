d322db10094b0d04a011b1c8ed780865577601f2
Drew Pfundstein <drew.pfundstein@salesloft.com>
2019-05-24 21:36:43 -0400

    fix mix file

--- projects/easy/series/mix.exs
+++ projects/easy/series/mix.exs
@@ -1,9 +1,9 @@
-defmodule Anagram.MixProject do
+defmodule Series.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :anagram,
+      app: :series,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
