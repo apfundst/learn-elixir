02a892f136fa8ea7a68fd2f40bcd8aa83fe6ebb9
Drew Pfundstein <drew.pfundstein@salesloft.com>
2019-06-18 19:20:21 -0400

    more functional solution

--- projects/easy/fizzbuzz/lib/fizzbuzz_solution.ex
+++ projects/easy/fizzbuzz/lib/fizzbuzz_solution.ex
@@ -9,11 +9,28 @@ defmodule Fizzbuzz.Solution do
   end
 
   def parse(num) do
-    case {rem(num, 3), rem(num, 5)} do
-      {0, 0} -> "FizzBuzz"
-      {0, _} -> "Fizz"
-      {_, 0} -> "Buzz"
-      _ -> num
+    {result, _} =
+      {"", num}
+      |> add_fizz()
+      |> add_buzz()
+
+    case result do
+      "" -> num
+      _ -> result
+    end
+  end
+
+  def add_fizz({entry, num}) do
+    case rem(num, 3) do
+      0 -> {entry <> "Fizz", num}
+      _ -> {entry, num}
+    end
+  end
+
+  def add_buzz({entry, num}) do
+    case rem(num, 5) do
+      0 -> {entry <> "Buzz", num}
+      _ -> {entry, num}
     end
   end
 end
