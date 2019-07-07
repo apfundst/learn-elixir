f24baca3ef9004407e3e8a056b3083bc0376bb00
Drew Pfundstein <drew.pfundstein@salesloft.com>
2019-07-07 15:23:22 -0400

    fix specs

--- projects/easy/reducer/test/reducer_test.exs
+++ projects/easy/reducer/test/reducer_test.exs
@@ -3,12 +3,21 @@ defmodule ReducerTest do
   doctest Reducer
 
   test "add_one" do
-    assert Reducer.run(%{action: 'add_one', number: 2}) == 3
+    assert Reducer.run(%{action: "add_one", number: 2}) == 3
   end
   test "subtract_one" do
-    assert Reducer.run(%{action: 'subtract_one', number: 2}) == 1
+    assert Reducer.run(%{action: "subtract_one", number: 2}) == 1
   end
   test "double" do
-    assert Reducer.run(%{action: 'double', number: 2}) == 4
+    assert Reducer.run(%{action: "double", number: 2}) == 4
+  end
+  test "other" do
+    assert Reducer.run(%{action: "other", number: 2}) == :error
+  end
+  test "another_one" do
+    assert Reducer.run(%{action: "another_one", number: 2}) == :error
+  end
+  test "and tuple" do
+    assert Reducer.run(%{action: {"and", "one"}, number: 2}) == :error
   end
 end
